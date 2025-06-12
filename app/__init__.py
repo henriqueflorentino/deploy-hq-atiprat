4b508cc9992afe4fb20010c14bdf332d1caebd46
Henrique Almeida Florentino <henriquealmeidaflorentino61@gmail.com>
2025-06-12 00:07:52 -0300

    pipeline: UPDATE 3

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,14 @@ import os
 
 app = Flask(__name__)
 
-# Configurações essenciais
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'uma-chave-secreta-muito-segura-123'
 
-# Configurações do MySQL
-app.config['MYSQL_HOST'] = 'db'
-app.config['MYSQL_USER'] = 'user'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'mydb'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Retorna resultados como dicionários
+
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' 
 
 mysql = MySQL(app)
 
